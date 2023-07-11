08b0daea0488ad442990c80e3f8238b05cb55ae5
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-07-11 21:42:39 +0900

    fix test

--- schema/sample/20211205102109-AddUsersTable.sql
+++ schema/sample/20211205102109-AddUsersTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `users`
 (
     `id`            int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '識別子',
     `name`          varchar(12)      NOT NULL COMMENT '名前',
-    `created_at`    datetime(6)      NOT NULL COMMENT 'レコード作成日時',
+    `created_at`    datetime(6)      NOT NULL COMMENT 'レコード作成日時1',
     `modified_at`   datetime(6)      NOT NULL COMMENT 'レコード修正日時',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
